26dea8ab567a6630ccc777fa4e4efcc91feb7122
Henry Baldwin <henrymbaldwin@proton.me>
2026-03-04 21:10:44 -0500

    af and if but not working?

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -946,6 +946,22 @@ require('lazy').setup({
     config = function() require('scrollbar').setup() end,
   },
 
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    opts = {
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+          },
+        },
+      },
+    },
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
