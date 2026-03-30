b21615026d55f4ff192b80d8b536b26609b1108d
Henry Baldwin <henrymbaldwin@proton.me>
2026-03-30 17:43:44 -0400

    Fix warnings.

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Henry's Home Manager config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Add this
