7c2caf919d372943ebc78eed7840799ca1b27162
Henry Baldwin <henrymbaldwin@proton.me>
2026-06-22 15:39:36 -0400

    Fix config.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -670,11 +670,11 @@ require('lazy').setup({
           },
         },
 
-        Some languages (like typescript) have entire language plugins that can be useful:
-           https://github.com/pmizio/typescript-tools.nvim
-
-        But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
+        -- Some languages (like typescript) have entire language plugins that can be useful:
+        --    https://github.com/pmizio/typescript-tools.nvim
+        --
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        -- ts_ls = {},
       }
 
       -- Ensure the servers and tools above are installed
